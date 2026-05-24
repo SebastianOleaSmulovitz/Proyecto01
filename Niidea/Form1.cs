bf4071fbbc54b4d8c8d6bc9b1b5e1a847c4ac4f1
sebas <sebas@DESKTOP-NA4S8NM>
2026-05-24 17:45:10 -0300

    Agregar archivos de proyecto.

---
+++ Niidea/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Niidea
+{
+    public partial class Form1 : Form
+    {
+        int Pass = 1234;
+        string Usuario = "Admin";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Contraseña = Convert.ToInt32(InputPass.Text);
+            String User = InputUser.Text;
+
+            if (Pass == Contraseña && Usuario == User)
+            {
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos.");
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InputUser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
