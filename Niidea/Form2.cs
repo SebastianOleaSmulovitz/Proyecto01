5a0bb159ba3edf34b5df7f50cfaa118e77d0f127
sebas <sebastian@gmail.com>
2026-05-24 21:19:48 -0300

    Base de datos inicio

--- Niidea/Form2.cs
+++ Niidea/Form2.cs
@@ -28,5 +28,12 @@ namespace Niidea
             form1.Show();
             this.Hide();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+            this.Hide();
+        }
     }
 }
---
+++ Niidea/Form3.cs
@@ -0,0 +1,78 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Niidea
+{
+    public partial class Form3 : Form
+    {
+
+        MySqlConnection connectionBD = new MySqlConnection("server=127.0.0.1;port=3306;user=root;password=;database=app;");
+        connectionBD.Open();
+        MySqlDataReader reader = null;
+        try{
+
+        }
+        catch (){
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form3_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
